a8ed22d5b8f0334ad2c77770c4bc99b73be69468
Matthieu COLLÉ <druidev+github@gmail.com>
2021-08-02 13:48:42 -0400

    chore(Contexts): removed merkle proof 2019

--- src/constants/contexts/contexts.ts
+++ src/constants/contexts/contexts.ts
@@ -1,11 +1,8 @@
 import BLOCKCERTSV1_2 from './blockcerts.v1.2.json';
 
-import MERKLE_PROOF_2019 from './merkle-proof-2019.json';
-
 import OPEN_BADGES from './open-badges.json';
 
 export default {
   BLOCKCERTSV1_2,
-  MERKLE_PROOF_2019,
   OPEN_BADGES
 };

370278eef549319e54c64bcb9644c661e8898fdb
Julien Fraichot <julien.fraichot@hyland.com>
2023-11-16 16:51:05 +0100

    chore(ci): decouple build tests from internet requests

--- test/build/verifier.test.build.js
+++ test/build/verifier.test.build.js
@@ -1,13 +1,17 @@
 import { VERIFICATION_STATUSES } from '../../src';
 import FIXTURES from '../fixtures';
+import { FakeXmlHttpRequest } from './mocks/FakeXmlHttpRequest';
 const verifier = require('../../dist/verifier');
+
+// @ts-expect-error we just mock the thing
+global.XMLHttpRequest = FakeXmlHttpRequest;
 
 describe('verifier build test suite', function () {
   it('works as expected with a v1 certificate', async function () {
     const certificate = new verifier.Certificate(FIXTURES.TestnetV1Valid);
     await certificate.init();
     const result = await certificate.verify();
-    expect(result.status).toBe(VERIFICATION_STATUSES.FAILURE);
-    expect(result.message).toBe('The address used to issue this Blockcerts does not belong to the claimed issuer.');
+    console.log(result);
+    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
   });
 });
